0c68fb5be29b4d7ea73f29d8eac3dccf792f625e
franckuestein <franckuestein@users.noreply.github.com>
2016-10-02 22:42:14 +0200

    Quit bug solved
    Completely closing app after pressing “Quit” button.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const electron = require('electron');
+const app = electron.app;
 var menubar = require('menubar');
 var mb = menubar({width: 400, height: 350, fullscreen: false})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    app.quit();
+});
 
 mb.on('ready', function ready () {
 	console.log('Running FoldingCoin ticker in your operating system.')
